761bba97f236f6033b28cc86509fbf801c2e7cb0
Ramon Ramos Cardoso da Silva <ramonramos@Ramons-MacBook-Pro.local>
2020-03-25 22:42:16 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', sessionController.create)
+
+routes.get('/ongs', ongController.index);
+routes.get('/incidents', incidentController.index);
+
+routes.get('/profile', profileController.index);
+
+routes.post('/ongs', ongController.create);
+routes.post('/incidents', incidentController.create);
+
+routes.delete('/incidents/:id', incidentController.delete)
+
+module.exports = routes;
